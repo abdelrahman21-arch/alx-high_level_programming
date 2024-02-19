3c79d4fde996afa17eda28d0484f14b5f2c38361
sweilam <boodyhussein12@gmail.com>
2024-02-18 16:20:25 -0800

    update

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""Module that retrieves and prints all states from a MySQL database using SQLAlchemy."""
+"""Module that retrieves and prints all states from a MySQL."""
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import State
 
-if if __name__ == "__main__":
+if __name__ == "__main__":
     #creates an db engine
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+    #Create a session factory
     Session = sessionmaker(bind=engine)
+    #Create a session object
     session = Session()
-
+    #Retrieve all states from the database and print their ids
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
