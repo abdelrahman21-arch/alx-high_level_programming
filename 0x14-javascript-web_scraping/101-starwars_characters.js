1b41030bb671aff9c1acb2ebba43aedf18c9a3b9
abdelrahman21-arch <61055709+abdelrahman21-arch@users.noreply.github.com>
2024-03-28 01:47:21 +0200

    Update 101-starwars_characters.js
    update

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,26 +4,26 @@ const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request(url, function (error, response, body) {
-  if (!error) {
-      const content = JSON.parse(body);
-      let characters = content.characters;
-    printCharacters(characters, 0);
-  }
-    else {
-	console.log(error);
-    }
+	if (!error) {
+	  const content = JSON.parse(body);
+	  let characters = content.characters;
+		printCharacters(characters, 0);
+	}
+	else {
+		console.log(error);
+	}
 });
 
 function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
-    }
-      else {
-	  console.log(error);
-      }
-  });
+	request(characters[index], function (error, response, body) {
+		if (!error) {
+			console.log(JSON.parse(body).name);
+			if (index + 1 < characters.length) {
+				printCharacters(characters, index + 1);
+			}
+		}
+		else {
+			console.log(error);
+		}
+	});
 }
